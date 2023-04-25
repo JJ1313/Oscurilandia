5c27aa7e5c94a1ee8e2bd02b875ad16161e60879
JJ1312 <yeiyeithirteen@gmail.com>
2023-04-24 22:41:13 -0400

    Agrega funcionalidad de agregar carros al tablero

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        int cantKromis = 3;
+        int cantCaguano = 5;
+        int cantTrupalla = 10;
         Tablero tablero = new Tablero();
-        tablero.display();
+        for(int i = 0; i<cantKromis; i++){
+            tablero.crearCarro("K");
+        }
+        for(int i=0; i<cantCaguano; i++){
+            tablero.crearCarro("C");
+        }
+        for(int i=0; i<cantTrupalla; i++){
+            tablero.crearCarro("T");
+        }
+        tablero.mostrarCarros();
+        tablero.mostrarMatriz();
     }
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.lang.Math;
 
 public class Tablero {
     private int cols = 15;
     private int rows = 15;
+    private int cantCarros = 0;
     private Carro[] carros = new Carro[18];
     private ArrayList<Huevo> huevos = new ArrayList<Huevo>();
     private String[][] matrix= new String[this.rows][this.cols];
@@ -26,13 +28,64 @@ public class Tablero {
     public void setHuevos(ArrayList<Huevo> huevos) {this.huevos = huevos;}
 
     //------ MOSTRAR TABLERO ------
-    public void display(){
+    public void mostrarMatriz(){
         for (String[] row: this.matrix) {
             for(String e: row){
                 System.out.print("[" + e + "]");
             }
             System.out.print("\n");
         }
+    }
+    public void crearCarro(String tipo){
+        boolean ubicado = false;
+        while(true){
+            int row = (int) Math.floor(Math.random() * 15);
+            int col = (int) Math.floor(Math.random() * 15);
+            if(tipo.equals("T")){
+                if(matrix[row][col].equals(" ")){
+                    carros[cantCarros] = new Trupalla();
+                    cantCarros++;
+                    matrix[row][col] = "T";
+                    break;
+                }
+            } else if (tipo.equals("K")) {
+                if(row+2 > 14){
+                    continue;
+                }
+                if(matrix[row][col].equals(" ") && matrix[row+1][col].equals(" ") && matrix[row+2][col].equals(" ")){
+                    carros[cantCarros] = new Kromi();
+                    cantCarros++;
+                    matrix[row][col] = "K";
+                    matrix[row+1][col] = "K";
+                    matrix[row+2][col] = "K";
+                    break;
+                }
+            } else if (tipo.equals("C")) {
+                if(col+1 > 14){
+                    continue;
+                }
+                if(matrix[row][col].equals(" ") && matrix[row][col+1].equals(" ")){
+                    carros[cantCarros] = new Caguano();
+                    cantCarros++;
+                    matrix[row][col] = "C";
+                    matrix[row][col+1] = "C";
+                    break;
+                }
+            }
+            else{
+                System.out.println("Tipo vehiculo no valido!!!");
+                break;
+            }
+
+        }
+    }
+    public void mostrarCarros(){
+        for (Carro carro: carros) {
+            carro.mostrarDatos();
+        }
+    }
+    public void lanzarHuevo(){
+
     }
 
 }
